da811ea5152b7555d80fac775ce693bb5a7fa264
DeltaDev <votinovilya174@gmail.com>
2025-12-08 21:25:39 +0500

    Update load_depends.sh

--- scripts/load_depends.sh
+++ scripts/load_depends.sh
@@ -3,5 +3,9 @@ echo "installing depends"
 echo "updating packages"
 sudo pacman -Syu
 echo "installing packages"
-sudo pacman -S nerd-font wofi hyprland fastfetch waybar nvim lua fish
+sudo pacman -S nerd-font wofi hyprland waybar vim nvim lua fish
 sudo pacman -S wireplumber pipewire pipewire-media-session xdg-desktop-portal-hyprland
+echo "installing hyprland utils"
+sudo pacman -S hyprpaper, hyprlock, hyprctl
+echo "installing utils"
+sudo pacman -S fastfetch htop cava yazi
