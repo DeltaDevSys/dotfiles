8066c61f9b5824a6f7986e05c30021664fa99794
DeltaDev <votinovilya174@gmail.com>
2025-12-08 20:47:00 +0500

    Update load_depends.sh

--- scripts/load_depends.sh
+++ scripts/load_depends.sh
@@ -4,3 +4,4 @@ echo "updating packages"
 sudo pacman -Syu
 echo "installing packages"
 sudo pacman -S nerd-font wofi hyprland fastfetch waybar nvim lua fish
+sudo pacman -S wireplumber pipewire pipewire-media-session xdg-desktop-portal-hyprland
