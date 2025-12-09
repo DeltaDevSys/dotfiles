e9f5e1a5b45606cc760aea542097d77ed1c41c76
DeltaDev <votinovilya174@gmail.com>
2025-12-09 19:07:25 +0500

    Update bufferline.lua

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,2 +1,20 @@
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+require("bufferline").setup{
+    options = {
+        custom_filter = function(buf_number)
+        if vim.bo[buf_number].filetype ~= "neo-tree" then
+            return true
+        end
+            return false
+        end,
+    
+        offsets = {
+            {
+                filetype = "neo-tree",
+                text = "File Explorer",
+                highlight = "Directory",
+                separator = true 
+            }
+        }
+    }
+}
