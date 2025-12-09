dbaab3cfcd883ece1f7013f20fe42368c7da57bb
DeltaDev <votinovilya174@gmail.com>
2025-12-09 19:41:16 +0500

    Update mappings.lua

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -1,5 +1,14 @@
 vim.g.mapleader = " "
 
--- NeoTree
 vim.keymap.set('n', '<leader>e', ':Neotree float focus<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CP>')
+
+vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true })
+
+vim.keymap.set({'n', 'v'}, '<C-x>', 'dd', { noremap = true, silent = true })
+
+vim.keymap.set({'n', 'i'}, '<C-z>', 'u', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<C-w>', ':q<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-w>', '<Esc>:q<CR>', { noremap = true, silent = true })
