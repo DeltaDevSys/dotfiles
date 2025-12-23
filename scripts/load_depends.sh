b919baad651f0a356ffd9fc9058f747e11356951
DeltaDev <votinovilya174@gmail.com>
2025-12-23 11:39:35 +0000

    Update load_depends.sh

--- scripts/load_depends.sh
+++ scripts/load_depends.sh
@@ -1,26 +1,67 @@
-# only for Arch and Arch-based
-echo "installing depends"
-echo "updating packages"
-sudo pacman -Syu
-echo "installing packages"
+clear
 
-echo "installing fonts"
-sudo pacman -S nerd-fonts 
-sudo pacman -S JetBrainsMono.zip
+echo "██████                          ██████                   █████                                      "
+echo "█    ██                      █  █    ██                  █   ██                                     "
+echo "█    ██  ████  ██   █   █████   █    ██  ████ █      █   █   ██  ██    █     ███  █  ██  ████ ████  "
+echo "█    ██  █  █  ██ █████ █   █   █    ██  █  █ █      █   █   ██ █  █ █████   █       ██  █  █ █     "
+echo "█    ██  ████  ██   █   █   █   █    ██  ████  █    █    █   ██ █  █   █   █████  █  ██  ████ ████  "
+echo "█    ██  █     ██   █   █   █   █    ██  █      █  █     █   ██ █  █   █     █    █  ██  █       █  "
+echo "██████   ████  ██   ██  ███████ ██████   ███     ██      ████    ██    ███   █    █  ██  ███  ████  "
+echo "                  "
 
-sudo pacman -S wofi hyprland waybar 
-sudo pacman -S vim nvim lua fish
-sudo pacman -S fcitx5 fcitx-configurator
-sudo pacman -S wireplumber pipewire pipewire-media-session xdg-desktop-portal-hyprland
-sudo pacman -S nm-applet
-echo "installing hyprland utils"
-sudo pacman -S hyprpaper, hyprlock, hyprctl
+while true; do
+  echo "starting script for installing depends: (y/n) "
+  read is_start
 
-echo "installing utils"
-sudo pacman -S fastfetch htop cava yazi zoxide
-sudo pacman -S exa bat
-yay -S whitesur-icon-theme-git
+  if [[ "$is_start" == "y" ]]; then
 
-echo "installing plug.vim"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo "installing depends"
+    echo "updating packages"
+    sudo pacman -Syu
+    echo "installing packages"
+
+    echo "installing git"
+    sudo pacman -S git
+
+    echo "installing fonts"
+    sudo pacman -S nerd-fonts 
+    sudo pacman -S JetBrainsMono.zip
+
+    echo "installing tool for screenshot"
+    sudo pacman -S grim
+    sudo pacman -S slurp
+
+    sudo pacman -S wofi hyprland waybar 
+    sudo pacman -S vim nvim lua fish
+    sudo pacman -S fcitx5 fcitx-configtool
+    sudo pacman -S wireplumber pipewire pipewire-media-session
+    sudo pacman -S xdg-desktop-portal-hyprland
+    sudo pacman -S nm-applet
+
+    echo "installing hyprland utils"
+    sudo pacman -S hyprpaper
+    sudo pacman -S hyprlock
+    sudo pacman -S hyprctl
+
+    echo "installing utils"
+    sudo pacman -S fastfetch htop cava yazi zoxide
+    sudo pacman -S exa bat
+    sudo pacman -S papirus-icon-theme
+
+    echo "installing plug.vim"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    echo "installing catppuccin kde theme"
+    git clone --depth=1 https://github.com/catppuccin/kde 
+    cd ~/kde
+    ./install.sh
+
+    echo "end installing depends"
+  elif [[ "$is_start" == "n" ]]; then
+    echo "script is not starting"
+    break
+  else
+    echo "choose only y or n"
+  fi
+done
